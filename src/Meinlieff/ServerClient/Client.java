b53d4711cd93c9b851eb4b80e77a924824c1e5f0
fklinck <francisklinck@gmail.com>
2019-04-03 17:03:47 +0200

    initial commit

---
+++ src/Meinlieff/ServerClient/Client.java
@@ -0,0 +1,12 @@
+package Meinlieff.ServerClient;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+}
